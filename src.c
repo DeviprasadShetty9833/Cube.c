93aecaef6c8e7ee23dff0fcdd999d44526e3a298
Deviprasad Shetty <auxiliaryds@gmail.com>
2025-05-16 21:27:01 +0530

    Update src.c

--- src.c
+++ src.c
@@ -1,38 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-int n,c,d,i,j;
+    int n;
+    printf("Enter the size: ");
+    scanf("%d", &n);
 
-printf("Enter the size: ");
-scanf("%d", &n);
+    int range = 2 * n - 1;
+    int d = 0;
 
-c=n-1;
-d=0;
-
-for(int i=1; i<=(2*n-1); i++)
+    for (int i = 1; i <= range; i++)
     {
-    for(int j=1; j<=(2*n-1); j++)
-       {
-       //printf("%d",d);
-          if (j>(n+d) && i<=n) // For Upper cut
-          {
-             printf("  ");
-             break;
-          }
-          else if (j<(n-d) && i>n) // For Lower cut
-          {
-             printf("  ");
-          }
-          else
-             printf("$ ");
-          }
-          printf("\n");
-          if(i<n)
-             d=d+1;
-          else
-             d=d-1;
-       
-     }
-return 0;
+        for (int j = 1; j <= range; j++)
+        {
+            // For upper half, Cut from the right
+            if (i <= n && j > n + d)
+                break;
+
+            // For lower half, Cut from the left
+            if (i > n && j < n - d)
+                printf("  ");
+            else
+                printf("$ ");
+        }
+
+        printf("\n");
+
+        // Increase width till middle row, then decrease
+        if(i < n)
+            d = d + 1;
+        else
+            d = d - 1;
+    }
+
+    return 0;
 }
